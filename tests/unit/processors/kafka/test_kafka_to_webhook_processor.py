d7d2947130e242113a9ac7e43a412d18a6a01ea5
Nimrod Marom <nimrodm@port.io>
2026-03-24 12:33:15 +0200

    remove redundant

--- tests/unit/processors/kafka/test_kafka_to_webhook_processor.py
+++ tests/unit/processors/kafka/test_kafka_to_webhook_processor.py
@@ -16,6 +16,7 @@ from streamers.kafka.kafka_streamer import KafkaStreamer
 from app.utils import sign_sha_256
 from tests.unit.processors.kafka.conftest import Consumer, terminate_consumer
 
+
 def _attach_wf_node_port_signature(node_run: dict, timestamp: int = 1713277889) -> None:
     invocation_method = node_run["config"]
     node_run_id = node_run["identifier"]
